d0e57c221e67e4d451d80e25d5aaec43b6bf3dd8
EnderKraken <cambkia@icloud.com>
2023-08-18 09:52:56 +0100

    d

--- sharedWorker.js
+++ sharedWorker.js
@@ -1,3 +1,7 @@
-self.onmessage = function(e) {
-  postMessage('Worker: '+e.data);
+self.onconnect = function(e) {
+  var port = e.ports[0];
+  port.onmessage = function(e) {
+    var result = e.data[0] * e.data[1];
+    port.postMessage('Test');
+  }
 }
